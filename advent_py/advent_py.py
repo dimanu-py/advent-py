b4dfbe45c3f93bedd69d2c6a2a5784b25fb13aa6
Diego Martinez Nuñez <diego.martinez@frenetic.ai>
2023-12-06 20:17:17 +0100

    Define basic folder structures for the projects
    The components folder will store different visual components.
    
    The views folder will store visual components, but more general and big.
    
    Finally, the styles folder will store the colors, fonts and styles.

--- advent_py/advent_py.py
+++ advent_py/advent_py.py
@@ -1,45 +1,25 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-from rxconfig import config
-
 import reflex as rx
 
-docs_url = "https://reflex.dev/docs/getting-started/introduction"
-filename = f"{config.app_name}/{config.app_name}.py"
 
+"""
+Reflex funciona de forma que si quiero definir una página tengo que crear una función que devuelva un componente.
 
-class State(rx.State):
-    """The app state."""
-
-    pass
-
-
+No vamos a crear backend, no hay estado, por lo que tendremos una página estática
+"""
 def index() -> rx.Component:
-    return rx.fragment(
-        rx.color_mode_button(rx.color_mode_icon(), float="right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", font_size="2em"),
-            rx.box("Get started by editing ", rx.code(filename, font_size="1em")),
-            rx.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": rx.color_mode_cond(
-                        light="rgb(107,99,246)",
-                        dark="rgb(179, 175, 255)",
-                    )
-                },
-            ),
-            spacing="1.5em",
-            font_size="2em",
-            padding_top="10%",
-        ),
+    return rx.box(
+
     )
 
 
-# Add state and page to the app.
-app = rx.App()
-app.add_page(index)
+app = rx.App(
+
+)
+
+app.add_page(
+    index,
+    title="Calendario de aDEViento 2023 | 24 días. 24 retos. 24 premios.",
+    description="Un calendario de adviento para desarrolladores python"
+)
+
 app.compile()
